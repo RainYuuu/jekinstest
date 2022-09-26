f0ac7e87c81ded37447deaa37982959d71101d7b
yugaoyuan <779066656@qq.com>
2022-09-26 18:09:08 +0800

    创建单元测试

---
+++ src/test/java/com/dcits/cn/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.dcits.cn.controller;
+
+import com.dcits.cn.entity.User;
+import com.dcits.cn.service.TestService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Description：
+ * @ClassName：TestController
+ * @Author:Mryu7
+ * @Date:2022/9/2617:59
+ */
+@SpringBootTest
+public class TestController {
+
+
+    @Autowired
+    private TestService testService;
+
+
+    @Test
+    public String test1(){
+
+        User user = testService.returnUser();
+
+        if(user==null){
+            return "{\"code:1\",\"msg:成功\"}";
+        }else{
+            return "{\"code:2\",\"msg:失败\"}";
+        }
+
+    }
+}
